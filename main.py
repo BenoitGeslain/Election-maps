421d548109a407244f6bdbef1b7f87ce29e191d2
benoitgeslain <benoitgeslain@gmail.com>
2024-06-18 19:56:01 +0200

    added comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 
 from bokeh.layouts import column
-from bokeh.models import GeoJSONDataSource, WheelZoomTool, PanTool, HoverTool, MultiChoice, AutocompleteInput, CustomJS
+from bokeh.models import GeoJSONDataSource, WheelZoomTool, PanTool, HoverTool, MultiChoice, AutocompleteInput, CustomJS, Range1d
 from bokeh.plotting import figure, show, output_file
 
 from utils.nfp import *
@@ -29,6 +29,8 @@ regionData = LoadRegionData()
 departementData = LoadDepartementData()
 candidates, NFPParties = LoadNFPData()
 
+
+# Filtering tool. TODO make them work with JS
 multiChoiceParties = MultiChoice(title="Partis", value=NFPParties, options=NFPParties)
 multiChoiceParties.js_on_change("value", CustomJS(code="""
     console.log('multiChoiceParties: value=' + this.value, this.toString())
@@ -42,6 +44,7 @@ autoCompleteDepartements.js_on_change("value", CustomJS(code="""
     console.log('multiChoiceParties: value=' + this.value, this.toString())
 """))
 
+# populating tooltip fields
 for i in range(len(data["features"])):
     circo = f'{data["features"][i]["properties"]["code_dpt"]}-{int(data["features"][i]["properties"]["num_circ"]):02d}'
     candidateData = candidates[circo]
@@ -54,17 +57,16 @@ for i in range(len(data["features"])):
     data['features'][i]['properties']['departement'] = data["features"][i]["properties"]["nom_dpt"]
     data['features'][i]['properties']['circo'] = f'{data["features"][i]["properties"]["num_circ"]}'
 
+# converting data to Bokeh GeoJSON format
 geoSource = GeoJSONDataSource(geojson=json.dumps(data))
 geoRegionSource = GeoJSONDataSource(geojson=json.dumps(regionData))
 geoDepartementSource = GeoJSONDataSource(geojson=json.dumps(departementData))
 
 output_file(filename="output/index.html", title="Front Populaire")
 hoverTool = HoverTool(tooltips=TOOLTIPS)
-# hoverTool.js_on_change("value", CustomJS(code="""
-#     console.log('multiChoiceParties: value=' + this.value, this.toString())
-# """))
+
 p = figure(background_fill_color="gainsboro", width=1000, height=1000, match_aspect=True,
-            x_range=(-7, 11), y_range=(40, 54),
+            x_range=Range1d(-7, 11, bounds=(-7.5, 11.5)), y_range=Range1d(40, 54, bounds=(39.5, 54.5)), # Setting plot initial range and max range
             tools=[PanTool(), WheelZoomTool(), hoverTool])
 
 circoPatches = p.patches('xs', 'ys', source=geoSource, line_width=0.5, line_color="white", fill_color='color')
